05a3f3d4f1318d3950c072fc01e8fca94bb8c844
Alexander <almerodevelop@gmail.com>
2022-05-03 16:39:48 +0300

    add 3

--- lab6.1/lab6.1.cpp
+++ lab6.1/lab6.1.cpp
@@ -1,9 +1,21 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <cmath>
+
+int factor(double num) {
+    long double factorial = 1.0;
+    for (int i = 1; i <= num; ++i) {
+        factorial *= i;
+    }
+    return factorial;
+}
 
 int main()
 {
     setlocale(LC_ALL, "ru");
-    // One
+
+    // One (Задание: разработать программу согласно заданию.)
     int num;
     std::cout << "Введите число:";
     std::cin >> num;
@@ -14,7 +26,8 @@ int main()
             std::cout <<"Делитель: " << i << "\n";
         }
     }
-    // Two
+
+    // Two (Задание: разработать программу согласно заданию.)
     int Number, sum(0);
     while (std::cin >> Number && Number != 0)
     {
@@ -22,6 +35,19 @@ int main()
             sum += Number;
     }
     std::cout << sum;
+
+    // Three (Задание: разработать программу вычисления суммы n членов ряда согласно условию задачи.)
+    double x, t = 1, s = t;
+    int n;
+    std::cout << "x=";
+    std::cin >> x;
+    std::cout << "n=";
+    std::cin >> n;
+    for (int i = 1; i <= n; i++) {
+        t *= x / i;
+        s += t * std::cos(M_PI / 4);
+    }
+    std::cout << "S =" << s << std::endl;
 
     return 0;
 }
