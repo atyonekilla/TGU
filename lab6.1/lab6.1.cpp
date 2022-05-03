5331b9f9cb11f69c04e2cb9d54489a81f3078e1d
Alexander <almerodevelop@gmail.com>
2022-05-03 14:41:21 +0300

    upl

---
+++ lab6.1/lab6.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int num;
+    std::cout << "Введите число:";
+    std::cin >> num;
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            std::cout <<"Делитель: " << i << "\n";
+        }
+    }
+    return 0;
+}
