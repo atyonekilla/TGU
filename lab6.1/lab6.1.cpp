2488350bb035693c5af9efbe5e0e07231d877e5e
Alexander <almerodevelop@gmail.com>
2022-05-03 14:50:20 +0300

    upd

--- lab6.1/lab6.1.cpp
+++ lab6.1/lab6.1.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     setlocale(LC_ALL, "ru");
+    // One
     int num;
     std::cout << "Введите число:";
     std::cin >> num;
@@ -13,5 +14,14 @@ int main()
             std::cout <<"Делитель: " << i << "\n";
         }
     }
+    // Two
+    int Number, sum(0);
+    while (std::cin >> Number && Number != 0)
+    {
+        if (Number > 0)
+            sum += Number;
+    }
+    std::cout << sum;
+
     return 0;
 }
